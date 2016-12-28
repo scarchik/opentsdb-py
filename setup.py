7ce1c30ef8c45268d939aad32641826801d07bf2
scarchik <scarchik@gmail.com>
2016-12-28 13:27:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,22 @@ setup(
     author='Sergey Suglobov',
     author_email='s.suglobov@gmail.com',
     packages=find_packages(),
-    keywords="opentsdb, tsdb",
+    keywords="opentsdb, tsdb, metrics",
     url='https://github.com/scarchik/opentsdb-py',
-    description='Python client for OpenTSDB',
+    description='Python3 client for OpenTSDB',
     install_requires=[
         "setuptools",
     ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
