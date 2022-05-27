bc68128208c0895caca787f35d5f4057f09dcdc7
Teung <dengyijian163@163.com>
2022-05-27 11:32:53 +0800

    feature: customize endpoint.

--- opentsdb/protocols/tests/test_http_urls.py
+++ opentsdb/protocols/tests/test_http_urls.py
@@ -1,12 +1,13 @@
-
 from opentsdb.protocols.http_connect import TSDBUrls
 
 
 def test_from_host_port():
-    url = TSDBUrls.from_host_and_port('127.0.0.1', 4242)
-    assert url.version == 'http://127.0.0.1:4242' + TSDBUrls.VERSION_ENDPOINT
+    url = TSDBUrls.from_host_and_port('127.0.0.1', 4242, version_endpoint="/api/version",
+                                      put_endpoint="/api/put?details=true")
+    assert url.put == 'http://127.0.0.1:4242/api/put?details=true'
 
 
 def test_from_uri():
-    url = TSDBUrls.from_uri('https://127.0.0.1:4242/opentsdb')
-    assert url.version == 'https://127.0.0.1:4242/opentsdb' + TSDBUrls.VERSION_ENDPOINT
+    url = TSDBUrls.from_uri('https://127.0.0.1:4242/opentsdb', version_endpoint="/api/version",
+                            put_endpoint="/api/put?details=true")
+    assert url.version == 'https://127.0.0.1:4242/opentsdb/api/version'
